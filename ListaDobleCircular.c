33595e0f4bf5f4f0dbffa00931f094bfe9aef316
LuisFC7 <luisitofc.28@gmail.com>
2019-11-19 12:14:20 -0600

    Has been finished all the program ListaDobleCircular.c

--- ListaDobleCircular.c
+++ ListaDobleCircular.c
@@ -6,7 +6,7 @@ void insertar();
 void imprimiro();
 void imprimira();
 void buscar();
-
+void eliminar();
 struct circular
 {
 
@@ -35,6 +35,9 @@ int main(void)
 	printf("\n*****");
 	imprimira();
 	buscar();
+	eliminar();
+	imprimiro();
+	imprimira();
 		
 	system("pause");
 	return 0;
@@ -152,4 +155,67 @@ void buscar()
 
 
 
+}
+
+void eliminar()
+{
+	struct circular *actual=(struct circular*)malloc(sizeof(struct circular));
+	actual=primero;
+
+	struct circular *anterior=(struct circular*)malloc(sizeof(struct circular));
+	anterior=NULL;
+
+	int nodobuscado=0;
+	int encontrado=0;
+
+	printf("\n\tEnter data to delete: ");
+	scanf("%d",&nodobuscado);
+
+	if(primero!=NULL)
+	{
+
+		do{
+			if(actual->datos == nodobuscado)
+			{
+
+
+
+			if(actual==primero)
+			{
+				primero=primero->siguiente;
+				primero->atras=ultimo;
+				ultimo->siguiente=primero;
+
+			}else if(actual==ultimo)
+				{
+					ultimo=anterior;
+					ultimo->siguiente=primero;
+					primero->atras=ultimo;
+
+
+
+				}else{
+
+					anterior->siguiente=actual->siguiente;
+					actual->siguiente->atras=anterior;
+				}
+			printf("\n\tNodo was deleted");
+			encontrado=1;
+
+			}
+			anterior=actual;
+			actual=actual->siguiente;
+
+			
+		}while(actual!=primero && encontrado != 1);
+
+		if(encontrado == 0)
+		{
+			printf("\n\tNode was not found");
+			
+		}else {
+			free(anterior);
+		}
+	
+	}
 }
