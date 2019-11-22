2654506433bf1f43727b7c5c38a34ab9e0cb305a
LuisFC7 <luisitofc.28@gmail.com>
2019-11-22 00:45:43 -0600

    Some functions has been corrected

--- Cola.c
+++ Cola.c
@@ -4,6 +4,7 @@
 
 void insertar();
 void imprimir();
+void eliminar();
 
 struct cola
 {
@@ -24,6 +25,8 @@ int main(void)
 	{
 	  	insertar();
 	}
+	imprimir();
+	eliminar();
 	imprimir();
 	return 0;
 
@@ -56,17 +59,69 @@ void imprimir()
 
 	if(primero!=NULL)
 	{
-		while(actual != NULL)
+		while(actual!=NULL)
 		{
-			printf("\n %d",actual->datos);
+			printf("\n%d",actual->datos);
 			actual=actual->siguiente;
+
 		}
+
 	}else{
 		printf("\nIs empty");
+
 	}
-
-
-
 }
 
 
+void eliminar()
+{
+	struct cola *actual=(struct cola*)malloc(sizeof(struct cola));
+	actual=primero;
+	
+	struct cola *anterior=(struct cola*)malloc(sizeof(struct cola));
+	anterior=NULL;
+
+	int nodobuscado=0;
+	int encontrado=0;
+	
+	printf("\n\tEnter node to delete: ");
+	scanf("%d",&nodobuscado);
+
+	if(primero!=NULL)
+	{
+		
+		while(actual!= NULL && encontrado!=1)
+		{
+			if(actual->datos == nodobuscado)
+			{
+				if(actual==primero)
+				{
+					primero=primero->siguiente;
+
+				}else if(actual==ultimo)
+					{
+					anterior->siguiente=NULL;
+					ultimo=anterior;
+	
+					}else{
+			
+						anterior->siguiente=actual->siguiente;
+	
+						}
+				encontrado=1;
+			}
+			
+			anterior=actual;
+			actual=actual->siguiente;
+		}
+
+		if(encontrado==0)
+		{
+			printf("\n\tNodo was not found");
+		}else{
+			free(anterior);
+		}
+
+	}
+
+}	
